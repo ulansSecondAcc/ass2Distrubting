6859cb96ddf900a3616e5cc669820478a01fe6b2
ulan414 <82831413+ulan414@users.noreply.github.com>
2022-10-27 22:39:28 +0600

    Update WebServer.java

--- dc-course-master/examples/WebServer/src/main/java/org/example/WebServer.java
+++ dc-course-master/examples/WebServer/src/main/java/org/example/WebServer.java
@@ -15,9 +15,6 @@ public class WebServer {
         // Port number for http request
         int port = args.length > 1 ? Integer.parseInt(args[1]) : 8080;
         // The maximum queue length for incoming connection
-        int queueLength = args.length > 2 ? Integer.parseInt(args[2]) : 50;
-        int numOfThreads = args.length > 1 ? Integer.parseInt(args[1]) : 4;
-        int numOfItems = (args.length > 2 ? Integer.parseInt(args[2]) : 100);
         //
 
             try (ServerSocket serverSocket = new ServerSocket(port, queueLength)) {
@@ -31,17 +28,7 @@ public class WebServer {
                         // To read input from the client
                         BufferedReader input = new BufferedReader(
                                 new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
-                        ThreaderQueue<BufferedReader> queue = new ThreaderQueue<>();
-                        for(int i = 0; i<numOfThreads; i++) {
-                            Threader th = new Threader(i, socket, queue);
-                            th.start();
-                        }
-                        //for(int i =0; i<numOfItems; i++) {
-                            queue.add_element(input);
-                        //}
-                        //for(int i = 0; i<numOfThreads; i++) {
-                            queue.add_element(null);
-                        //}
+                        
 
                 }
             } catch (IOException ex) {
