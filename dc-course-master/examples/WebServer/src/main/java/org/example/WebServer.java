7d498325db7ab191f4bb1d5d8f2eaf3dd1eb0b7c
ulan414 <82831413+ulan414@users.noreply.github.com>
2022-10-28 22:48:40 +0600

    Update WebServer.java

--- dc-course-master/examples/WebServer/src/main/java/org/example/WebServer.java
+++ dc-course-master/examples/WebServer/src/main/java/org/example/WebServer.java
@@ -4,8 +4,6 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 /**
  * Simple web server.
@@ -15,32 +13,41 @@ public class WebServer {
         // Port number for http request
         int port = args.length > 1 ? Integer.parseInt(args[1]) : 8080;
         // The maximum queue length for incoming connection
-        //
+        int queueLength = args.length > 2 ? Integer.parseInt(args[2]) : 50;;
 
-            try (ServerSocket serverSocket = new ServerSocket(port, queueLength)) {
-                System.out.println("Web Server is starting up, listening at port " + port + ".");
-                System.out.println("You can access http://localhost:" + port + " now.");
-                while (true) {
-                    // Make the server socket wait for the next client request
-                        Socket socket = serverSocket.accept();
+        try (ServerSocket serverSocket = new ServerSocket(port, queueLength)) {
+            System.out.println("Web Server is starting up, listening at port " + port + ".");
+            System.out.println("You can access http://localhost:" + port + " now.");
 
-                        InputStream inputStream = socket.getInputStream();
-                        // To read input from the client
-                        BufferedReader input = new BufferedReader(
-                                new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
-                        
+            while (true) {
+                // Make the server socket wait for the next client request
+                Socket socket = serverSocket.accept();
 
+                System.out.println("Got connection!");
+
+                // To read input from the client
+                BufferedReader input = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+
+                try {
+                    // Get request
+                    HttpRequest request = HttpRequest.parse(input);
+
+                    // Process request
+                    Processor proc = new Processor(socket, request);
+                    proc.process();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
                 }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            } finally {
-                System.out.println("Server has been shutdown!");
             }
         }
-
-
-
-
-
-
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            System.out.println("Server has been shutdown!");
+        }
+    }
 }
